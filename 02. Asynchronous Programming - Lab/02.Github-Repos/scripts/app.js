acfc111f4cb8790cc3aa94f8fa761f733e4373fb
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-11-22 13:33:23 +0200

    working on js applications

---
+++ 02. Asynchronous Programming - Lab/02.Github-Repos/scripts/app.js
@@ -0,0 +1,34 @@
+"use strict";
+
+function loadRepos() {
+  document.getElementById("repos").innerHTML = "";
+
+  let username = document.getElementById("username").value;
+
+  if (!username) {
+    return;
+  }
+
+  let url = `https://api.github.com/users/${username}/repos`;
+
+  fetch(url)
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error(`Error: ${response.status} -  ${response.statusText}`);
+      }
+    })
+    .then((repositories) => {
+      repositories.forEach((repo) => {
+        const listItem = document.createElement("li");
+        listItem.innerHTML = `<strong>${repo.full_name}</strong>: <a href="${repo.html_url}" target ="_blank">${repo.html_url}</a>`;
+        document.getElementById("repos").appendChild(listItem);
+      });
+    })
+    .catch((error) => {
+      let errorItem = document.createElement("li");
+      errorItem.textContent = error.message;
+      document.getElementById("repos").appendChild(errorItem);
+    });
+}
