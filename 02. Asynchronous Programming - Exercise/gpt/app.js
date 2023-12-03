286b2eb1d4f47eba9d52f26f7979d002e0593192
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-12-03 12:08:42 +0200

    ajax

--- 02. Asynchronous Programming - Exercise/gpt/app.js
+++ 02. Asynchronous Programming - Exercise/gpt/app.js
@@ -1,1 +1,11 @@
 var xhttp = new XMLHttpRequest();
+
+xhttp.onreadystatechange = function () {
+  if (this.readyState == 4 && this.status == 200) {
+    // The request is complete, and the response is ready
+    console.log(this.responseText);
+  }
+};
+
+xhttp.open("GET", "https://jsonplaceholder.typicode.com/posts/1", true);
+xhttp.send();
