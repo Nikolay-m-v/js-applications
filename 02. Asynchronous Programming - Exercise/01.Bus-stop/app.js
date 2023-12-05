bd3ab4ee034d9e07b30e1ad8675af532e13c48de
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-12-05 20:01:11 +0200

    async

--- 02. Asynchronous Programming - Exercise/01.Bus-stop/app.js
+++ 02. Asynchronous Programming - Exercise/01.Bus-stop/app.js
@@ -1,27 +1,3 @@
 "use strict";
 
-function getInfo() {
-  const inputElement = document.getElementById("stopId").value;
-  const ulElement = document.getElementById("buses");
-  const divElement = document.getElementById("stopName");
-
-  fetch(`http://localhost:3030/jsonstore/bus/businfo/${inputElement}`)
-    .then((response) => response.json())
-    .then((data) => {
-      let buses = data.buses;
-      let name = data.name;
-
-      divElement.textContent = name;
-      ulElement.innerHTML = "";
-
-      Object.keys(buses).forEach((bus) => {
-        let liElement = document.createElement("li");
-        liElement.textContent = `Bus ${bus} arrives in ${buses[bus]} minutes`;
-        ulElement.appendChild(liElement);
-      });
-    })
-    .catch((error) => {
-      divElement.textContent = "Error";
-      ulElement.innerHTML = "";
-    });
-}
+function getInfo() {}
