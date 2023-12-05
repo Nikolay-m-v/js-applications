16711a7f7d2b328f0cc4d8416a0de1757f882521
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-12-05 20:11:49 +0200

    async

--- 02. Asynchronous Programming - Exercise/01.Bus-stop/app.js
+++ 02. Asynchronous Programming - Exercise/01.Bus-stop/app.js
@@ -1,3 +1,23 @@
 "use strict";
 
-function getInfo() {}
+function getInfo() {
+  const divElement = document.getElementById("stopName");
+  const ulElement = document.getElementById("buses");
+  const inputElement = document.getElementById("stopId").value;
+
+  fetch(`http://localhost:3030/jsonstore/bus/businfo/${stopId}`)
+    .then((response) => response.json())
+    .then((data) => {
+      let buses = data.buses;
+      let name = data.name;
+
+      divElement.textContent = name;
+      ulElement.innerHTML = "";
+
+      Object.keys(buses).forEach((bus) => {
+        let liElement = document.createElement("li");
+        liElement.textContent = `Bus ${Bus} arrives in ${buses[bus]} minutes`;
+        ulElement.appendChild(liElement);
+      });
+    });
+}
