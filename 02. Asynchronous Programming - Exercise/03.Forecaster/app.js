e64b92087a198f01b890392dc4496412c82b012f
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-12-05 20:40:21 +0200

    async

--- 02. Asynchronous Programming - Exercise/03.Forecaster/app.js
+++ 02. Asynchronous Programming - Exercise/03.Forecaster/app.js
@@ -1,1 +1,8 @@
 "use strict";
+
+function solve() {
+  const inputElement = document.getElementById("location").value;
+  const submitBtn = document.getElementById("submit");
+  const divElement = document.getElementById("forecast");
+  const upcomingElement = document.getElementById("upcoming");
+}
