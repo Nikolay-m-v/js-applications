9c9d620907f6c20e501b348ffd086312dad6040e
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-12-05 20:42:39 +0200

    async

--- 02. Asynchronous Programming - Exercise/03.Forecaster/app.js
+++ 02. Asynchronous Programming - Exercise/03.Forecaster/app.js
@@ -1,5 +1,10 @@
 "use strict";
 
+const getData = async (url) => {
+  const data = await fetch(`http://localhost:3030/jsonstore/forecaster/${uri}`);
+  if (!data.ok) throw new Error();
+};
+
 function solve() {
   const inputElement = document.getElementById("location").value;
   const submitBtn = document.getElementById("submit");
