41a691b5fdedcb8c1f812cac10d351c07abc2715
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-12-07 13:02:03 +0200

    ajax practice

--- 02. Asynchronous Programming - Exercise/03.Forecaster/app.js
+++ 02. Asynchronous Programming - Exercise/03.Forecaster/app.js
@@ -1,13 +1,29 @@
 "use strict";
 
-const getData = async (url) => {
-  const data = await fetch(`http://localhost:3030/jsonstore/forecaster/${uri}`);
-  if (!data.ok) throw new Error();
-};
+// const getData = async (url) => {
+//   const data = await fetch(`http://localhost:3030/jsonstore/forecaster/${uri}`);
+//   if (!data.ok) throw new Error();
+// };
 
 function solve() {
   const inputElement = document.getElementById("location").value;
   const submitBtn = document.getElementById("submit");
   const divElement = document.getElementById("forecast");
-  const upcomingElement = document.getElementById("upcoming");
+  const upcomingDayDiv = document.getElementById("upcoming");
+  const currentDayDiv = document.getElementById("current");
+  let baseUrl = "http://localhost:3030/jsonstore/forecaster";
+  let sunny = "&#x2600";
+  let partlySunny = "&#x26C5";
+  let overcast = "&#x2601";
+  let rain = "&#x2614";
+  let degrees = "&#176";
+  let code = "";
+
+  let divElementCurrent = document.createElement("div");
+  let divElementUpcoming = document.createElement("div");
+
+  submitBtn.addEventListener("click", (e) => {
+    divElementCurrent.innerHTML = "";
+    divElementUpcoming.innerHTML = "";
+  });
 }
