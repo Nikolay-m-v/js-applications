8983fa26be35ed87744fb53f6e62ab4d00c504c9
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-12-19 13:09:11 +0200

    ajax practice

--- 02. Asynchronous Programming - Exercise/06.Blog/app.js
+++ 02. Asynchronous Programming - Exercise/06.Blog/app.js
@@ -1,7 +1,70 @@
 "use strict";
 
-function solution() {
-  const btnLoadPosts = document.getElementById("btnLoadPosts");
+document.addEventListener("DOMContentLoaded", () => {
+  const loadPostsBtn = document.getElementById("btnLoadPosts");
+  const viewPostBtn = document.getElementById("btnViewPost");
+  const postsDropdown = document.getElementById("posts");
+  const postTitle = document.getElementById("post-title");
+  const postBody = document.getElementById("post-body");
+  const postComments = document.getElementById("post-comments");
 
-  btnLoadPosts.addEventListener("click", () => {});
-}
+  loadPostsBtn.addEventListener("click", loadPosts);
+  viewPostBtn.addEventListener("click", viewPost);
+
+  async function loadPosts() {
+    try {
+      const response = await fetch(
+        "http://localhost:3030/jsonstore/blog/posts"
+      );
+      const data = await response.json();
+
+      postsDropdown.innerHTML = "";
+
+      for (const postId in data) {
+        const option = document.createElement("option");
+        option.value = postId;
+        option.textContent = data[postId].title;
+        postsDropdown.appendChild(option);
+      }
+    } catch (error) {
+      console.error("Error loading posts:", error);
+    }
+  }
+
+  async function viewPost() {
+    const selectedPostId = postsDropdown.value;
+
+    try {
+      // Get selected post
+      const postResponse = await fetch(
+        `http://localhost:3030/jsonstore/blog/posts/${selectedPostId}`
+      );
+      const postData = await postResponse.json();
+
+      postTitle.textContent = postData.title;
+      postBody.textContent = postData.body;
+
+      // Get comments for the selected post
+      const commentsResponse = await fetch(
+        "http://localhost:3030/jsonstore/blog/comments"
+      );
+      const commentsData = await commentsResponse.json();
+
+      // Filter comments for the current post
+      const postCommentsData = Object.values(commentsData).filter(
+        (comment) => comment.postId === selectedPostId
+      );
+
+      // Display comments
+      postComments.innerHTML = "";
+
+      postCommentsData.forEach((comment) => {
+        const li = document.createElement("li");
+        li.textContent = comment.text;
+        postComments.appendChild(li);
+      });
+    } catch (error) {
+      console.error("Error viewing post:", error);
+    }
+  }
+});
