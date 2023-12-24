d3c892541e8b1f574da1d2829fc0954a205be243
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-12-24 12:00:48 +0200

    practice

--- Additional Tasks/Async - Exercise/Bus Schedule/app.js
+++ Additional Tasks/Async - Exercise/Bus Schedule/app.js
@@ -3,16 +3,39 @@
 function solve() {
   const departBtn = document.getElementById("depart");
   const arriveBtn = document.getElementById("arrive");
-  const info = document.getElementById("info");
+  const infoElement = document.getElementById("infoElement");
 
   let busStop = {
     next: "depot",
   };
 
-  departBtn
-    .addEventListener("click", () => {
-      fetch(`http://localhost:3030/jsonstore/bus/schedule/${busStop.next}`);
-    })
-    .then((response) => response.json())
-    .then((data) => {});
+  departBtn.addEventListener("click", depart);
+
+  function depart() {
+    departBtn.disabled = true;
+
+    fetch(`http://localhost:3030/jsonstore/bus/schedule/${busStop.next}`)
+      .then((response) => response.json())
+      .then((data) => {
+        busStop = JSON.parse(JSON.stringify(data));
+        infoElement.textContent = `Next Stop is ${busStop.name}`;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    arriveBtn.disabled = false;
+  }
+
+  function arrive() {
+    arriveBtn.disabled = true;
+
+    fetch;
+  }
+
+  return {
+    depart,
+    arrive,
+  };
 }
+
+let result = solve();
