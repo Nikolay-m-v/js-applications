8a2573330b375350bc2f75fde499ad779f2d1e28
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-12-24 11:44:01 +0200

    practice

--- Additional Tasks/Async - Exercise/Bus Stop/app.js
+++ Additional Tasks/Async - Exercise/Bus Stop/app.js
@@ -1,3 +1,30 @@
 "use strict";
 
-function getInfo() {}
+function getInfo() {
+  const stopId = document.getElementById("stopId").value;
+  const submitBtn = document.getElementById("submit");
+  const stopName = document.getElementById("stopName");
+  const ulElement = document.getElementById("buses");
+
+  submitBtn.addEventListener("click", () => {
+    fetch(`http://localhost:3030/jsonstore/bus/businfo/${stopId}`)
+      .then((response) => response.json())
+      .then((data) => {
+        let buses = data.buses;
+        let name = data.name;
+
+        stopName.textContent = name;
+        ulElement.innerHTML = "";
+
+        Object.keys(buses).forEach((bus) => {
+          let liElement = document.createElement("li");
+          liElement.textContent = `Bus ${bus} arrives in ${buses[bus]} minutes`;
+          ulElement.appendChild(liElement);
+        });
+      })
+      .catch((error) => {
+        stopName.textContent = `Error`;
+        ulElement.innerHTML = "";
+      });
+  });
+}
