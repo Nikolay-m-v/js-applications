081196b0c772c951c4154e36fb29ad8caefe8355
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-12-26 17:40:32 +0200

    ajax redo

--- Additional Tasks/Async - Exercise/Forecaster/app.js
+++ Additional Tasks/Async - Exercise/Forecaster/app.js
@@ -7,12 +7,14 @@ function solve() {
   const currentElement = document.getElementById("current");
   const upcomingElement = document.getElementById("upcoming");
 
-  submitBtn.addEventListener("click", () => {
+  submitBtn.addEventListener("click", (event) => {
+    console.log("works");
+    event.preventDefault();
     getWeather();
   });
 
   function getWeather() {
-    const locationName = locationElement.value();
+    const locationName = locationElement.value;
 
     if (!locationName) {
       displayError();
@@ -29,15 +31,61 @@ function solve() {
           displayError();
           return;
         }
-      });
 
-    Promise.all([
-      fetch(
-        `http://localhost:3030/jsonstore/forecaster/today/${location.code}`
-      ).then((res) => res.json()),
-      fetch(
-        `http://localhost:3030/jsonstore/forecaster/upcoming/${location.code}`
-      ).then((res) => res.json()),
-    ]);
+        fetch(
+          `http://localhost:3030/jsonstore/forecaster/today/${location.code}`
+        )
+          .then((response) => response.json())
+          .then((currentData) => {
+            return fetch(
+              `http://localhost:3030/jsonstore/forecaster/upcoming/${location.code}`
+            )
+              .then((response) => response.json())
+              .then((upcomingData) => {
+                displayForecast(currentData, upcomingData);
+              });
+          })
+          .catch(() => {
+            displayError();
+          });
+      })
+      .catch(() => {
+        displayError();
+      });
+  }
+
+  function displayForecast(currentData, upcomingData) {
+    currentElement.innerHTML = `
+    <div class="label">${currentData.name}</div>
+    <div class="forecast-info">
+      <p>Low: ${currentData.forecast.low}&#176;C</p>
+      <p>High: ${currentData.forecast.high}&#176;C</p>
+      <p>Condition: ${currentData.forecast.condition}</p>
+    </div>
+  `;
+
+    upcomingElement.innerHTML = `
+    <div class="label">${upcomingData.name}</div>
+    ${upcomingData.forecast
+      .map(
+        (day) => `
+        <div class="forecast-info">
+          <p>Low: ${day.low}&#176;C</p>
+          <p>High: ${day.high}&#176;C</p>
+          <p>Condition: ${day.condition}</p>
+        </div>
+      `
+      )
+      .join("")}
+  `;
+    forecastElement.style.display = "block";
+  }
+
+  function displayError() {
+    currentElement.innerHTML = `<p>Error</p>`;
+    upcomingElement.innerHTML = `<p>Error</p>`;
+    forecastElement.style.display = "block";
   }
 }
+
+solve();
