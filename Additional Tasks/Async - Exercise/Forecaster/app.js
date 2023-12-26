1fc73853237f4684b2bf06a46fe41e664793a6fe
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-12-26 17:25:51 +0200

    ajax redo

--- Additional Tasks/Async - Exercise/Forecaster/app.js
+++ Additional Tasks/Async - Exercise/Forecaster/app.js
@@ -1,1 +1,17 @@
 "use strict";
+
+function solve() {
+  const locationElement = document.getElementById("location");
+  const submitBtn = document.getElementById("submit");
+  const forecastElement = document.getElementById("forecast");
+  const currentElement = document.getElementById("current");
+  const upcomingElement = document.getElementById("upcoming");
+
+  submitBtn.addEventListener("click", () => {
+    getWeather();
+  });
+
+  function getWeather() {
+    const locationName = locationElement.value();
+  }
+}
