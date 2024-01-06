20d2dfa6583af743ee652c51068cf677dd40a5a4
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2024-01-06 12:56:38 +0200

    ajax

--- Additional Tasks/Async - Exercise/Accordion/app1.js
+++ Additional Tasks/Async - Exercise/Accordion/app1.js
@@ -1,14 +1,6 @@
 "use strict";
 
 async function solution() {
-  const showMoreBtn = document.getElementById(
-    "ee9823ab-c3e8-4a14-b998-8c22ec246bd3"
-  );
-  const extraDivToShow = document.getElementsByClassName("extra")[0];
-  // let articleListUrl = "http://localhost:3030/jsonstore/advanced/articles/list";
-  // let articleId = "insert_article_id";
-  // let articleIdUrl = `http://localhost:3030/jsonstore/advanced/articles/details/${articleId}`;
-
   try {
     let url = "http://localhost:3030/jsonstore/advanced/articles/list";
     let response = await fetch(url);
@@ -22,7 +14,7 @@ async function solution() {
     data.forEach((article) => {
       let articleElement = document.createElement("div");
       articleElement.classList.add("accordion");
-      articleElement.innerHtml = `<div class="head">
+      articleElement.innerHTML = `<div class="head">
       <span>${article.title}</span>
       <button class="button" id="${article._id}" onclick="moreOnclick(event)">More</button>
       </div>
@@ -34,5 +26,36 @@ async function solution() {
     console.log(error);
   }
 
-  async function moreOnclick(event) {}
+  async function moreOnclick(event) {
+    try {
+      let currentTarget = event.currentTarget;
+      let url =
+        "http://localhost:3030/jsonstore/advanced/articles/details/" +
+        currentTarget.id;
+      let parent = currentTarget.parentNode.parentNode;
+      let extraDiv = parent.querySelector("div.extra");
+
+      let response = await fetch(url);
+
+      if (response.ok === false) {
+        throw new Error("Error");
+      }
+
+      let data = await response.json();
+
+      extraDiv.innerHTML = `<p>${data.content}</p>`;
+
+      if (currentTarget.textContent === "More") {
+        currentTarget.textContent = "Less";
+        extraDiv.style.display = "block";
+      } else {
+        currentTarget.textContent = "More";
+        extraDiv.style.display = "none";
+      }
+    } catch (error) {
+      throw new Error("Error fetching");
+    }
+  }
 }
+
+solution();
