79cc34929c82633d748147239723394d00c65847
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2024-01-06 13:13:22 +0200

    ajax

--- Additional Tasks/Async - Exercise/Accordion/app1.js
+++ Additional Tasks/Async - Exercise/Accordion/app1.js
@@ -25,36 +25,36 @@ async function solution() {
   } catch (error) {
     console.log(error);
   }
+}
 
-  async function moreOnclick(event) {
-    try {
-      let currentTarget = event.currentTarget;
-      let url =
-        "http://localhost:3030/jsonstore/advanced/articles/details/" +
-        currentTarget.id;
-      let parent = currentTarget.parentNode.parentNode;
-      let extraDiv = parent.querySelector("div.extra");
+async function moreOnclick(event) {
+  try {
+    let currentTarget = event.currentTarget;
+    let url =
+      "http://localhost:3030/jsonstore/advanced/articles/details/" +
+      currentTarget.id;
+    let parent = currentTarget.parentNode.parentNode;
+    let extraDiv = parent.querySelector("div.extra");
 
-      let response = await fetch(url);
+    let response = await fetch(url);
 
-      if (response.ok === false) {
-        throw new Error("Error");
-      }
-
-      let data = await response.json();
-
-      extraDiv.innerHTML = `<p>${data.content}</p>`;
-
-      if (currentTarget.textContent === "More") {
-        currentTarget.textContent = "Less";
-        extraDiv.style.display = "block";
-      } else {
-        currentTarget.textContent = "More";
-        extraDiv.style.display = "none";
-      }
-    } catch (error) {
-      throw new Error("Error fetching");
+    if (response.ok === false) {
+      throw new Error("Error");
     }
+
+    let data = await response.json();
+
+    extraDiv.innerHTML = `<p>${data.content}</p>`;
+
+    if (currentTarget.textContent === "More") {
+      currentTarget.textContent = "Less";
+      extraDiv.style.display = "block";
+    } else {
+      currentTarget.textContent = "More";
+      extraDiv.style.display = "none";
+    }
+  } catch (error) {
+    throw new Error("Error fetching");
   }
 }
 
