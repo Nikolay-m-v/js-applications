6448bb568370719d11661487c8b2cb4303b19a92
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2024-01-06 12:39:43 +0200

    ajax

--- Additional Tasks/Async - Exercise/Accordion/app1.js
+++ Additional Tasks/Async - Exercise/Accordion/app1.js
@@ -1,52 +1,38 @@
 "use strict";
 
-const showMoreBtn = document.getElementById(
-  "ee9823ab-c3e8-4a14-b998-8c22ec246bd3"
-);
-const extraDivToShow = document.getElementsByClassName("extra")[0];
-let articleListUrl = "http://localhost:3030/jsonstore/advanced/articles/list";
-let articleId = "insert_article_id";
-let articleIdUrl = `http://localhost:3030/jsonstore/advanced/articles/details/${articleId}`;
+async function solution() {
+  const showMoreBtn = document.getElementById(
+    "ee9823ab-c3e8-4a14-b998-8c22ec246bd3"
+  );
+  const extraDivToShow = document.getElementsByClassName("extra")[0];
+  // let articleListUrl = "http://localhost:3030/jsonstore/advanced/articles/list";
+  // let articleId = "insert_article_id";
+  // let articleIdUrl = `http://localhost:3030/jsonstore/advanced/articles/details/${articleId}`;
 
-async function getArticleDetails() {
-  fetch(articleListUrl)
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("Error fetching data");
-      }
-      return response.json();
-    })
-    .then((data) => {
-      console.log(data);
-      console.log(data[0]);
-      for (let i = 0; i < data.length; i++) {
-        const objData = data[i];
-        const { _id, title } = objData;
-        articleId = objData._id;
-        getArticleIdDetails();
-      }
+  try {
+    let url = "http://localhost:3030/jsonstore/advanced/articles/list";
+    let response = await fetch(url);
+
+    if (response.ok === false) {
+      throw new Error("Error fetching article list");
+    }
+
+    let data = await response.json();
+
+    data.forEach((article) => {
+      let articleElement = document.createElement("div");
+      articleElement.classList.add("accordion");
+      articleElement.innerHtml = `<div class="head">
+      <span>${article.title}</span>
+      <button class="button" id="${article._id}" onclick="moreOnclick(event)">More</button>
+      </div>
+      <div class="extra"></div>`;
+      let main = document.getElementById("main");
+      main.appendChild(articleElement);
     });
-}
-
-async function getArticleIdDetails() {
-  fetch(articleIdUrl)
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("Error fetchin data");
-      }
-    })
-    .then((data) => {});
-}
-
-showMoreBtn.addEventListener("click", () => {
-  if (extraDivToShow.style.display === "none") {
-    showMoreBtn.textContent = "MORE";
-    extraDivToShow.style.display = "block";
-  } else {
-    showMoreBtn.textContent = "LESS";
-    extraDivToShow.style.display = "none";
+  } catch (error) {
+    console.log(error);
   }
-});
 
-getArticleDetails();
-//// fix
+  async function moreOnclick(event) {}
+}
