4eaa812654966f351030e642082dceb2b1e0adc0
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2024-01-09 11:52:19 +0200

    practice

--- Additional Tasks/Async - Exercise/Blog/app.js
+++ Additional Tasks/Async - Exercise/Blog/app.js
@@ -3,8 +3,11 @@
 let postsUrl = "http://localhost:3030/jsonstore/blog/posts";
 let commentsUrl = "http://localhost:3030/jsonstore/blog/comments";
 const btnLoadPosts = document.getElementById("btnLoadPosts");
+const postsDropdown = document.getElementById("posts");
+const btnViewPost = document.getElementById("btnViewPost");
 
 btnLoadPosts.addEventListener("click", getPostsData);
+btnViewPost.addEventListener("click", getCommentsData);
 
 async function getPostsData() {
   try {
@@ -20,6 +23,19 @@ async function getPostsData() {
       let optionElement = document.createElement("option");
       optionElement.value = postId;
       optionElement.textContent = data[postId].title;
+      postsDropdown.appendChild(optionElement);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function getCommentsData() {
+  try {
+    let response = await fetch(commentsUrl);
+
+    if (response.ok === false) {
+      throw new Error("Error fetching data");
     }
   } catch (error) {
     console.log(error);
