5bb36b3f04d9a5d53113a2cb187ee6f4b54a66bf
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2024-01-18 12:44:04 +0200

    more practice

--- Additional Tasks/Async - Exercise/Locked Profile/app1.js
+++ Additional Tasks/Async - Exercise/Locked Profile/app1.js
@@ -17,6 +17,37 @@ async function lockedProfile() {
 
     let data = await response.json();
     console.log(data);
+
+    Object.values(data).forEach((profile) => {
+      let divElement = document.createElement("div");
+      let buttonElement = document.createElement("button");
+      buttonElement.innerHTML = "Show More";
+      divElement.classList.add("profile");
+      divElement.innerHTML = `<img src="./iconProfile2.png" class="userIcon">
+            <label>Lock</label>
+            <input type="radio" name="user${profile._id}Locked" value="lock" checked="">
+            <label>Unlock</label>
+            <input type="radio" name="user${profile._id}Locked" value="unlock"><br>
+            <hr>
+            <label>Username</label>
+            <input type="text" name="user${profile._id}Username" value=${profile.username} disabled="" readonly="">
+            <div id="user${profile._id}HiddenFields">
+            <hr>
+            <label>Email:</label>
+            <input type="email" name="user${profile._id}Email" value=${profile.email} disabled="" readonly="">
+            <label>Age:</label>
+            <input type="email" name="user${profile._id}Age" value=${profile.age} disabled="" readonly="">
+            </div>`;
+
+      let main = document.querySelector("main");
+      main.innerHTML = "";
+      divElement.appendChild(buttonElement);
+      main.appendChild(divElement);
+
+      buttonElement.addEventListener("click", reveal);
+
+      function reveal() {}
+    });
   } catch (error) {
     console.log(error);
   }
