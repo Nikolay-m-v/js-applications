e75651bdfe8fd6917dd60f418c305378e3dd09d9
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2024-01-18 11:50:28 +0200

    more practice

---
+++ Additional Tasks/Async - Exercise/Bus Stop/app1.js
@@ -0,0 +1,8 @@
+"use strict";
+
+function getInfo() {
+  const stopId = document.getElementById("stopId");
+  const submitButton = document.getElementById("submit");
+  const stopName = document.getElementById("stopName");
+  const buses = document.getElementById("buses");
+}
