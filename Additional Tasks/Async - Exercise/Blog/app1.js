1a9ca492e7e0c95a543b750bff0e2022ea2d5f2d
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2024-01-20 12:22:18 +0200

    practice

--- Additional Tasks/Async - Exercise/Blog/app1.js
+++ Additional Tasks/Async - Exercise/Blog/app1.js
@@ -3,4 +3,28 @@
 (async function main() {
   let postsUrl = `http://localhost:3030/jsonstore/blog/posts`;
   let commentsUrl = `http://localhost:3030/jsonstore/blog/comments`;
+
+  const loadPostsBtn = document.getElementById("btnLoadPosts");
+  const dropDownPosts = document.getElementById("posts");
+  const viewPostBtn = document.getElementById("btnViewPost");
+  const postTitleElement = document.getElementById("post-title");
+  const postBodyElement = document.getElementById("post-body");
+  const postComments = document.getElementById("post-comments");
+
+  loadPostsBtn.addEventListener("click", () => {
+    loadPosts();
+  });
+
+  async function loadPosts() {
+    const response = await fetch(postsUrl);
+    const data = await response.json();
+    console.log(data);
+
+    for (const postId in data) {
+      const optionElement = document.createElement("option");
+      optionElement.value = postId;
+      optionElement.textContent = data[postId].title;
+      dropDownPosts.appendChild(optionElement);
+    }
+  }
 })();
