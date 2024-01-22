a160db220adbbd10bf877a43af38499e76323fa4
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2024-01-22 15:26:55 +0200

    videos of promises and await fetch

--- Additional Tasks/Async - Exercise/Bus Stop/app2.js
+++ Additional Tasks/Async - Exercise/Bus Stop/app2.js
@@ -1,3 +1,21 @@
 "use strict";
 
-function getInfo() {}
+function getInfo() {
+  const stopName = document.getElementById("stopName");
+  const buses = document.getElementById("buses");
+  const submitBtn = document.getElementById("submit");
+  const stopId = document.getElementById("stopId");
+  let url = `http://localhost:3030/jsonstore/bus/businfo/`;
+
+  submitBtn.addEventListener("click", async () => {
+    const currentStopId = stopId.value;
+    const response = await fetch(url + currentStopId);
+    const data = await response.json();
+    stopName.textContent = data.name;
+    let buses = data.buses;
+
+    Object.keys(buses).forEach((bus) => {
+      let liElement = document.createElement("li");
+    });
+  });
+}
