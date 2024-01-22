72d6fb5400ac7d06a4e72683e0771a41dd9114df
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2024-01-22 15:31:31 +0200

    additional practice

--- Additional Tasks/Async - Exercise/Bus Stop/app2.js
+++ Additional Tasks/Async - Exercise/Bus Stop/app2.js
@@ -2,7 +2,7 @@
 
 function getInfo() {
   const stopName = document.getElementById("stopName");
-  const buses = document.getElementById("buses");
+  const busesUlElement = document.getElementById("buses");
   const submitBtn = document.getElementById("submit");
   const stopId = document.getElementById("stopId");
   let url = `http://localhost:3030/jsonstore/bus/businfo/`;
@@ -13,9 +13,12 @@ function getInfo() {
     const data = await response.json();
     stopName.textContent = data.name;
     let buses = data.buses;
+    busesUlElement.innerHTML = "";
 
     Object.keys(buses).forEach((bus) => {
       let liElement = document.createElement("li");
+      liElement.textContent = `Bus ${bus} arrives in ${buses[bus]} minutes.`;
+      busesUlElement.appendChild(liElement);
     });
   });
 }
