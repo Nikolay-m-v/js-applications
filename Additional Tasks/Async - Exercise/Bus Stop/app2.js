e5ea1f4a348d39116f234ba31be79c049f168099
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2024-01-22 15:34:27 +0200

    additional practice

--- Additional Tasks/Async - Exercise/Bus Stop/app2.js
+++ Additional Tasks/Async - Exercise/Bus Stop/app2.js
@@ -9,16 +9,24 @@ function getInfo() {
 
   submitBtn.addEventListener("click", async () => {
     const currentStopId = stopId.value;
-    const response = await fetch(url + currentStopId);
-    const data = await response.json();
-    stopName.textContent = data.name;
-    let buses = data.buses;
-    busesUlElement.innerHTML = "";
+    try {
+      const response = await fetch(url + currentStopId);
+      if (!response.ok) {
+        throw new Error("Failed to fetch bus info.");
+      }
 
-    Object.keys(buses).forEach((bus) => {
-      let liElement = document.createElement("li");
-      liElement.textContent = `Bus ${bus} arrives in ${buses[bus]} minutes.`;
-      busesUlElement.appendChild(liElement);
-    });
+      const data = await response.json();
+      stopName.textContent = data.name;
+      let buses = data.buses;
+      busesUlElement.innerHTML = "";
+
+      Object.keys(buses).forEach((bus) => {
+        let liElement = document.createElement("li");
+        liElement.textContent = `Bus ${bus} arrives in ${buses[bus]} minutes.`;
+        busesUlElement.appendChild(liElement);
+      });
+    } catch (error) {
+      console.log(error);
+    }
   });
 }
