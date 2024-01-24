1f69300562809b6088ac36600051a8c5ea55530f
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2024-01-24 11:29:46 +0200

    new tasks

--- 03. Remote Data and Authentication - Exercise/01.Messenger/app1.js
+++ 03. Remote Data and Authentication - Exercise/01.Messenger/app1.js
@@ -1,45 +1,63 @@
 "use strict";
 
-let urlRequests = `http://localhost:3030/jsonstore/messenger`;
+function main() {
+  let urlRequests = `http://localhost:3030/jsonstore/messenger`;
+  document.querySelector("#refresh").addEventListener("click", displayComments);
+  document.querySelector("#submit").addEventListener("click", addComment);
 
-const sendBtn = document.getElementById("submit");
-const refreshBtn = document.getElementById("refresh");
+  sendBtn.addEventListener("click", () => {
+    addComment();
+  });
 
-sendBtn.addEventListener("click", () => {
-  addComment();
-});
+  async function addComment() {
+    let authorName = document.querySelector('[name="author"]');
+    let content = document.querySelector('[name="content"]');
 
-async function addComment() {
-  let authorName = document.querySelector('[name="author"]');
-  let content = document.querySelector('[name="content"]');
-
-  if (authorName.value === "" || content.value === "") {
-    return;
-  }
-
-  try {
-    const response = await fetch(url, {
-      method: "post",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        author: authorName.value.trim(),
-        content: content.value.trim(),
-      }),
-    });
-
-    if (!response.ok) {
-      throw new Error("Error");
+    if (authorName.value === "" || content.value === "") {
+      return;
     }
 
-    const data = response.json();
-    return data;
-  } catch (error) {
-    console.log(error);
+    try {
+      const response = await fetch(urlRequests, {
+        method: "post",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          author: authorName.value.trim(),
+          content: content.value.trim(),
+        }),
+      });
+
+      if (!response.ok) {
+        throw new Error("Error");
+      }
+
+      const data = response.json();
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
+
+    authorName.value = "";
+    content.value = "";
+    displayComments();
   }
 
-  authorName.value = "";
-  content.value = "";
-  displayComments();
+  async function displayComments() {
+    try {
+      const response = await fetch(urlRequests);
+      if (!response.ok) {
+        console.log("Error");
+      }
+
+      const data = await response.json();
+
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 }
+
+main();
