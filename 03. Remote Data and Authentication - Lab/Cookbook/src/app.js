70d6cdc1ec77ddeaac39a6dce9d4d2dc277325e3
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2024-01-24 10:57:57 +0200

    new tasks

--- 03. Remote Data and Authentication - Lab/Cookbook/src/app.js
+++ 03. Remote Data and Authentication - Lab/Cookbook/src/app.js
@@ -1,1 +1,29 @@
 "use strict";
+
+let urlRecipes = `http://localhost:3030/data/recipes?select=_id%2Cname%2Cimg`;
+let urlSelectedRecipe = `http://localhost:3030/data/recipes/:id`;
+let urlCreateNewRecipePost = `http://localhost:3030/data/recipes`;
+
+const registerBtn = document.getElementById("");
+
+function displayHeader() {
+  const header = {
+    loggedIn: document.getElementById("user"),
+    loggedOut: document.getElementById(`guest`),
+  };
+  const logoutBtn = document.getElementById("logoutBtn");
+
+  if (sessionStorage.accessToken) {
+    header.loggedIn.style.display = "inline-block";
+    header.loggedOut.style.display = "none";
+    logoutBtn.addEventListener("click", logout);
+  } else {
+    header.loggedIn.style.display = "none";
+    header.loggedOut.style.display = "inline-block";
+    logoutBtn.removeEventListener("click", logout);
+  }
+}
+
+document.addEventListener(`DOMContentLoaded`, () => {
+  displayHeader();
+});
