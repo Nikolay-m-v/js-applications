c9f249bd79512b1c363d88dac1ba3ccb21c26155
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2024-01-24 11:09:29 +0200

    new tasks

---
+++ 03. Remote Data and Authentication - Exercise/01.Messenger/app1.js
@@ -0,0 +1,38 @@
+"use strict";
+
+let urlRequests = `http://localhost:3030/jsonstore/messenger`;
+
+const sendBtn = document.getElementById("submit");
+const refreshBtn = document.getElementById("refresh");
+
+sendBtn.addEventListener("click", () => {
+  addComment();
+});
+
+async function addComment() {
+  let authorName = document.querySelector('[name="author"]');
+  let content = document.querySelector('[name="content"]');
+
+  if (authorName.value === "" || content.value === "") {
+    return;
+  }
+
+  const response = await fetch(url, {
+    method: "post",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      author: authorName.value.trim(),
+      content: content.value.trim(),
+    }),
+  });
+
+  if (!response.ok) {
+    throw new Error("Error");
+  }
+
+  const data = response.json();
+
+  return data;
+}
