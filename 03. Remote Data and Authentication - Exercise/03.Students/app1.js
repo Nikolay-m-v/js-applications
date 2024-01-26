f41adae0b06f9879857c72fbf25c889458749bc8
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2024-01-26 12:21:13 +0200

    remote data and authentication

---
+++ 03. Remote Data and Authentication - Exercise/03.Students/app1.js
@@ -0,0 +1,11 @@
+"use strict";
+
+function main() {
+  let url = "http://localhost:3030/jsonstore/collections/students";
+  let students = {
+    FirstName: "",
+    LastName: "",
+    FacultyNumber: "",
+    GradeNumber: "",
+  };
+}
