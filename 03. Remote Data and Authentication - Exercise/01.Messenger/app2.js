a870c825a397f59b3c49637d003a2accf30320bd
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2024-01-30 16:22:26 +0200

    remote data and authentication practice

--- 03. Remote Data and Authentication - Exercise/01.Messenger/app2.js
+++ 03. Remote Data and Authentication - Exercise/01.Messenger/app2.js
@@ -2,11 +2,20 @@
 (function main() {
   const url = ` http://localhost:3030/jsonstore/messenger`;
 
-  sendButton.addEventListener("click", () => {
-    createNewMessage();
-  });
+  async function createNewMessage(author, content) {
+    const newMessage = { author, content };
 
-  function createNewMessage() {}
+    const response = await fetch(url, {
+      method: `POST`,
+      headers: {
+        "Content-Type": `application/json`,
+      },
+      body: JSON.stringify(newMessage),
+    });
+
+    const newlyCreatedMessage = await response.json();
+    return newlyCreatedMessage;
+  }
 
   function getElements() {
     const sendButton = document.getElementById("submit");
@@ -17,6 +26,25 @@
 
     return {
       sendButton,
+      refreshButton,
+      inputAuthor,
+      inputMessage,
+      messagesBox,
     };
   }
+
+  async function displayAllMessages() {
+    const response = await fetch(url);
+  }
+
+  function eventHandling() {
+    const els = getElements();
+
+    els.sendButton.addEventListener("click", () => {
+      createNewMessage();
+      displayAllMessages();
+    });
+  }
+
+  eventHandling();
 })();
--- 03. Remote Data and Authentication - Exercise/01.Messenger/main-by-william.js
+++ 03. Remote Data and Authentication - Exercise/01.Messenger/main-by-william.js
@@ -1,122 +1,122 @@
-'use strict';
+"use strict";
 
-(async function (){
-    const messagesUrl = 'http://localhost:3030/jsonstore/messenger';	
+(async function () {
+  const messagesUrl = "http://localhost:3030/jsonstore/messenger";
 
-    /* ====== DATA MANIPULATION ====== */
-    async function getAllMessages() {
-        const response = await fetch(messagesUrl);
-        const messages = await response.json();
+  /* ====== DATA MANIPULATION ====== */
+  async function getAllMessages() {
+    const response = await fetch(messagesUrl);
+    const messages = await response.json();
 
-        return messages;
-    }
+    return messages;
+  }
 
-    async function createMessage(author, content) {
-        const newMessageContent = { author, content };
+  async function createMessage(author, content) {
+    const newMessageContent = { author, content };
 
-        const response = await fetch(messagesUrl, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(newMessageContent)
-        });
-
-        const newlyCreatedMessage = await response.json();
-
-        return newlyCreatedMessage;
-    }
-    /* ====== DATA MANIPULATION ====== */
-
-    /* ====== DOM MANIPULATION ====== */
-    function getElements() {
-        const elements = {
-            allMessagesTextArea: document.getElementById('messages'),
-            authorInput: document.querySelector('input[name="author"]'),
-            contentInput: document.querySelector('input[name="content"]'),
-            submitButton: document.getElementById('submit'),
-            refreshButton: document.getElementById('refresh')
-        };
-
-        return elements;
-    }
-
-    function collectMessageData(els) {
-        const author = els.authorInput.value;
-        const content = els.contentInput.value
-
-        const data = {
-            author,
-            content
-        };
-
-        return data;
-    }
-
-    function clearInputs(els) {
-        els.authorInput.value = '';
-        els.contentInput.value = '';
-    }
-
-    function clearTextArea(els) {
-        els.allMessagesTextArea.value = '';
-    }
-
-    function validateMessage(message) {
-        const isValid = (message.author !== '') && (message.content !== '');
-
-        return isValid;
-    }
-
-    async function submitNewMessage(els) {
-        const newMessage = collectMessageData(els);
-
-        if (validateMessage(newMessage) === false) {
-            return;
-        }
-
-        await createMessage(newMessage.author, newMessage.content);
-
-        clearInputs(els);
-    }
-
-    function formatMessage(message) {
-        const formattedMessage = `${message.author}: ${message.content}\n`;
-
-        return formattedMessage;
-    }
-
-    function updateMessagesTextAreaScroll(els) {
-        els.allMessagesTextArea.scrollTop = els.allMessagesTextArea.scrollHeight;
-    }
-
-    async function displayAllMessages(els) {
-        clearTextArea(els);
-        
-        const allMessages = await getAllMessages();
-        
-        Object.values(allMessages).forEach(message => {
-            els.allMessagesTextArea.value += formatMessage(message);
-        });
-
-        updateMessagesTextAreaScroll(els);
-    }
-
-    function setupEvents() {
-        const els = getElements();
-
-        els.submitButton.addEventListener('click', () => {
-            submitNewMessage(els);
-            displayAllMessages(els);
-        });
-
-        els.refreshButton.addEventListener('click', async () => {
-            displayAllMessages(els);
-        });
-    }
-
-    document.addEventListener('DOMContentLoaded', async () => {
-        setupEvents();
+    const response = await fetch(messagesUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newMessageContent),
     });
-    /* ====== DOM MANIPULATION ====== */
-}());
+
+    const newlyCreatedMessage = await response.json();
+
+    return newlyCreatedMessage;
+  }
+  /* ====== DATA MANIPULATION ====== */
+
+  /* ====== DOM MANIPULATION ====== */
+  function getElements() {
+    const elements = {
+      allMessagesTextArea: document.getElementById("messages"),
+      authorInput: document.querySelector('input[name="author"]'),
+      contentInput: document.querySelector('input[name="content"]'),
+      submitButton: document.getElementById("submit"),
+      refreshButton: document.getElementById("refresh"),
+    };
+
+    return elements;
+  }
+
+  function collectMessageData(els) {
+    const author = els.authorInput.value;
+    const content = els.contentInput.value;
+
+    const data = {
+      author,
+      content,
+    };
+
+    return data;
+  }
+
+  function clearInputs(els) {
+    els.authorInput.value = "";
+    els.contentInput.value = "";
+  }
+
+  function clearTextArea(els) {
+    els.allMessagesTextArea.value = "";
+  }
+
+  function validateMessage(message) {
+    const isValid = message.author !== "" && message.content !== "";
+
+    return isValid;
+  }
+
+  async function submitNewMessage(els) {
+    const newMessage = collectMessageData(els);
+
+    if (validateMessage(newMessage) === false) {
+      return;
+    }
+
+    await createMessage(newMessage.author, newMessage.content);
+
+    clearInputs(els);
+  }
+
+  function formatMessage(message) {
+    const formattedMessage = `${message.author}: ${message.content}\n`;
+
+    return formattedMessage;
+  }
+
+  function updateMessagesTextAreaScroll(els) {
+    els.allMessagesTextArea.scrollTop = els.allMessagesTextArea.scrollHeight;
+  }
+
+  async function displayAllMessages(els) {
+    clearTextArea(els);
+
+    const allMessages = await getAllMessages();
+
+    Object.values(allMessages).forEach((message) => {
+      els.allMessagesTextArea.value += formatMessage(message);
+    });
+
+    updateMessagesTextAreaScroll(els);
+  }
+
+  function setupEvents() {
+    const els = getElements();
+
+    els.submitButton.addEventListener("click", () => {
+      submitNewMessage(els);
+      displayAllMessages(els);
+    });
+
+    els.refreshButton.addEventListener("click", async () => {
+      displayAllMessages(els);
+    });
+  }
+
+  document.addEventListener("DOMContentLoaded", async () => {
+    setupEvents();
+  });
+  /* ====== DOM MANIPULATION ====== */
+})();
