f5ae3369e9ae4b5880d17c4e062463cf8b39f8ac
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2024-02-04 12:04:37 +0200

    students redo

--- 03. Remote Data and Authentication - Exercise/03.Students/app2.js
+++ 03. Remote Data and Authentication - Exercise/03.Students/app2.js
@@ -5,6 +5,8 @@
   let table = document.querySelector("#results tbody");
   let form = document.querySelector("form");
 
+  form.addEventListener("submit", createStudent);
+
   async function loadStudents() {
     const response = await fetch(url);
 
@@ -18,9 +20,35 @@
         document.createElement("td", record.facultyNumber),
         document.createElement("td", record.grade)
       );
+      table.appendChild(student);
     });
-    console.log(data);
   }
 
+  async function createStudent(event) {
+    event.preventDefault();
+    const formData = FormData(form);
+    const studentData = {};
+    formData.forEach((value, key) => {
+      studentData[key] = value;
+    });
+    const response = await fetch(url, {
+      method: "post",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(studentData),
+    });
+
+    const createdStudent = await response.json();
+
+    let student = document.createElement("tr");
+    ["firstName", "lastName", "facultyNumber", "grade"].forEach((field) => {
+      let cell = document.createElement("td");
+      cell.textContent = createdStudent[field];
+      student.appendChild(cell);
+    });
+
+    table.appendChild(student);
+  }
   loadStudents();
 })();
