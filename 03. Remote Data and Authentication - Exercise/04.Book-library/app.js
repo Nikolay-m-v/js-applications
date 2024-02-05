cf64cb9ac75b6ea2c75632ad1872040fc8bedf1d
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2024-02-05 12:40:09 +0200

    more auth and data remote practice

--- 03. Remote Data and Authentication - Exercise/04.Book-library/app.js
+++ 03. Remote Data and Authentication - Exercise/04.Book-library/app.js
@@ -3,15 +3,55 @@
 (function main() {
   const booksUrl = `http://localhost:3030/jsonstore/collections/books`;
   const loadButton = document.getElementById("loadBooks");
+  const form = document.querySelector("form");
+  const createBookButton = document.getElementById("submit");
 
   loadButton.addEventListener("click", () => {
     getBooks();
   });
+
+  createBookButton.addEventListener("click", (event) => {
+    createNewBook(event);
+  });
+
+  function checkInputValues() {
+    const nameInput = document.querySelector(`input[name="title"]`);
+    const authorInput = document.querySelector(`input[name="author"]`);
+
+    if (nameInput.value === "" || authorInput.value === "") {
+      console.log("Fill all inputs!");
+      return;
+    }
+  }
 
   async function getBooks() {
     const response = await fetch(booksUrl);
     const data = await response.json();
 
     console.log(data);
+  }
+
+  async function createNewBook(event) {
+    event.preventDefault();
+    const bookData = {};
+    checkInputValues();
+
+    const formData = new FormData(form);
+    formData.forEach((value, key) => {
+      bookData[key] = value;
+    });
+
+    console.log(formData);
+
+    const response = await fetch(booksUrl, {
+      method: "post",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(bookData),
+    });
+
+    const newBook = await response.json();
+    console.log(newBook);
   }
 })();
