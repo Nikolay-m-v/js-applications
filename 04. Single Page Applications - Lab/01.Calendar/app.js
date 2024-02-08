3ce3e8e13c7f90f5142a0537bb0d7339524526d8
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2024-02-08 15:51:34 +0200

    single page applications

--- 04. Single Page Applications - Lab/01.Calendar/app.js
+++ 04. Single Page Applications - Lab/01.Calendar/app.js
@@ -2,16 +2,39 @@
 
 document.addEventListener("DOMContentLoaded", () => {
   const yearsContainer = document.getElementById("years");
+  const monthsContainer = document.querySelectorAll(".monthCalendar");
+  const daysContainer = document.querySelectorAll(".daysCalendar");
+
+  monthsContainer.forEach((month) => {
+    month.style.display = "none";
+  });
+
+  daysContainer.forEach((day) => {
+    day.style.display = "none";
+  });
 
   function initCalendar() {
-    const dayElement = yearsContainer.querySelectorAll(".day");
+    const dayElements = yearsContainer.querySelectorAll(".day");
 
-    dayElement.forEach((dateElement) => {
-      dateElement.addEventListener("click", (event) => {
+    dayElements.forEach((dateElement) => {
+      dateElement.addEventListener("click", () => {
         const selectedYear = dateElement.textContent.trim();
-        console.log(selectedYear);
+        showMonths(selectedYear);
       });
     });
+  }
+
+  function showMonths(year) {
+    const selectedYearContainer = document.getElementById(`year-${year}`);
+    if (selectedYearContainer) {
+      monthsContainer.forEach((month) => {
+        month.style.display = "none";
+      });
+
+      selectedYearContainer.style.display = "block";
+    } else {
+      console.log(`Year container for ${year} not found.`);
+    }
   }
 
   initCalendar();
