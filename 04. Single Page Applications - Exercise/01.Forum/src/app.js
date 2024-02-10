5dfbdecdd1f2763d457cb90338de31d25f8b7fb9
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2024-02-10 12:38:57 +0200

    practice

--- 04. Single Page Applications - Exercise/01.Forum/src/app.js
+++ 04. Single Page Applications - Exercise/01.Forum/src/app.js
@@ -1,1 +1,34 @@
 "use strict";
+
+(function main() {
+  const postsUrl = `http://localhost:3030/jsonstore/collections/myboard/posts`;
+  const titleName = document.getElementById("topicName");
+  const userName = document.getElementById("username");
+  const postContent = document.getElementById("postText");
+  const postButton = document.querySelector(".public");
+  const cancelButton = document.querySelector(".cancel");
+
+  postButton.addEventListener("click", () => {
+    submitNewPost();
+  });
+
+  cancelButton.addEventListener("click", () => {
+    titleName.value = "";
+    userName.value = "";
+    postContent.value = "";
+  });
+
+  function validateFormValues() {
+    let isValid = true;
+    if (
+      titleName.value === "" ||
+      userName.value === "" ||
+      postContent.value === ""
+    ) {
+      isValid = false;
+      return isValid;
+    }
+  }
+
+  async function submitNewPost() {}
+})();
