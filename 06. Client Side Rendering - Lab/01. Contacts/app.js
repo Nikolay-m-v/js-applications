db4fc91d15c4be5368128dcedbdc1032597e56d4
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2024-02-22 12:50:33 +0200

    architecture and testing

--- 06. Client Side Rendering - Lab/01. Contacts/app.js
+++ 06. Client Side Rendering - Lab/01. Contacts/app.js
@@ -1,3 +1,17 @@
 "use strict";
 
 import { contacts } from "./contacts.js";
+
+const contactTemplate = (contact) => html`<div class="contact card">
+  <div>
+    <i class="far fa-user-circle gravatar"></i>
+  </div>
+  <div class="info">
+    <h2>Name: ${contact.name}</h2>
+    <button @click=${showDetails} class="detailsBtn">Details</button>
+    <div class="details" id=${contact.id}>
+      <p>Phone number: ${contact.phoneNumber}</p>
+      <p>Email: ${contact.email}</p>
+    </div>
+  </div>
+</div>`;
