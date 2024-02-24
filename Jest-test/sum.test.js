4418aa3595a39189c40e18ebf20a72aafe22b1ff
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2024-02-24 13:47:59 +0200

    jest testing

--- Jest-test/sum.test.js
+++ Jest-test/sum.test.js
@@ -15,7 +15,12 @@ const sum = require(`./sum`);
 //   expect(data).toEqual({ one: 1, two: 2 });
 // });
 
-test("null is falsy", () => {
-  const n = null;
-  expect(n).toBeFalsy();
+// test("zero is falsy", () => {
+//   const n = 0;
+//   expect(n).toBeFalsy();
+// });
+
+test("one is truthy", () => {
+  const n = 1;
+  expect(n).toBeTruthy();
 });
