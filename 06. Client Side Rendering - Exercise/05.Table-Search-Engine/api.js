87a8561dfcf6ef5c7b517e18d415993649dc88a5
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2024-02-29 13:48:51 +0200

    client side rendering

---
+++ 06. Client Side Rendering - Exercise/05.Table-Search-Engine/api.js
@@ -0,0 +1,8 @@
+"use strict";
+
+let url = "http://localhost:3030/jsonstore/advanced/table";
+
+export async function getAllStudents() {
+  const response = await fetch(url);
+  return await response.json();
+}
