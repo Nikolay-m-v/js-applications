1b75101360c9e2d42e35decb83f9865efaeda443
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2024-03-04 11:14:45 +0200

    fixing nodule pathing

--- 06. Client Side Rendering - Exercise/03.Search-in-List/app.js
+++ 06. Client Side Rendering - Exercise/03.Search-in-List/app.js
@@ -1,6 +1,9 @@
 "use strict";
 
-import { html, render } from "./node_modules/lit-html/lit-html.js";
+import {
+  html,
+  render,
+} from "../03.Search-in-List/node_modules/lit-html/lit-html.js";
 import { towns } from "./towns.js";
 
 let cardTemplate = html`<ul>
