2ef7b916a934037a54cd92a7f5e8bd91d9f1bc4f
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2024-03-12 14:21:14 +0200

    node practices

---
+++ 06. Client Side Rendering - Exercise/02.HTTP-Status-Cats/app1.js
@@ -0,0 +1,36 @@
+"use strict";
+
+import { html, render } from "../../node_modules/lit-html/lit-html.js";
+import { cats } from "./catSeeder.js";
+
+const ulElement = document.createElement("ul");
+const allCatsSection = document.getElementById("allCats");
+allCatsSection.appendChild(ulElement);
+
+let cardTemplate = (cat) =>
+  html`<li>
+    <img src="./images/${cat.imageLocation}.jpg" width="250px" height="250px" />
+    <div class="info">
+      <button class="showBtn" @click=${onClick}>Show status code</button>
+      <div class="status" style="display: none" id="${cat.id}">
+        <h4>Status code: ${cat.statusCode}</h4>
+        <p>${cat.statusMessage}</p>
+      </div>
+    </div>
+  </li>`;
+
+function onClick(event) {
+  let cat = event.target.parentNode;
+  let result = cat.querySelector(".status").style.display;
+
+  if (result === "block") {
+    cat.querySelector(".status").style.display = "none";
+  } else {
+    cat.querySelector(".status").style.display = "block";
+  }
+
+  console.log(cat, result);
+}
+
+let result = cats.map(cardTemplate);
+render(result, allCatsSection);
