d6b92783d1c5c97fc2e2e7f2ce2e893ed9182ca7
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2024-03-16 12:09:26 +0200

    more nodules practice

--- 06. Client Side Rendering - Exercise/04.Fill-Dropdown/dropdown2.js
+++ 06. Client Side Rendering - Exercise/04.Fill-Dropdown/dropdown2.js
@@ -7,47 +7,50 @@ import {
 
 const url = `http://localhost:3030/jsonstore/advanced/dropdown`;
 
-async function getAllItems() {
-  const response = await fetch(url);
+document.addEventListener("DOMContentLoaded", async () => {
+  async function getAllItems() {
+    const response = await fetch(url);
 
-  const data = await response.json();
+    const data = await response.json();
 
-  return data;
-}
-
-let items = Object.values(await getAllItems());
-
-let cardTemplate = html`${items.map((item) => {
-  html`<option value=${item._id}>${item.text}</option>`;
-})}`;
-
-render(cardTemplate);
-
-document
-  .querySelector('input[type="submit"]')
-  .addEventListener("click", addItem);
-
-async function addItem(event) {
-  event.preventDefault();
-
-  let text = document.getElementById("itemText").value;
-
-  const response = await fetch(url, {
-    method: "POST",
-    headers: { "Content-type": "application/json" },
-    body: JSON.stringify({ text }),
-  });
-
-  let data = await response.json();
-
-  items.push(data);
-
-  if (response.ok) {
-    cardTemplate = html`${items.map(
-      (item) => html`<option value=${item._id}>${item.text}</option>`
-    )}`;
-    render(cardTemplate, main);
+    return data;
   }
-}
 
-getAllItems();
+  let items = Object.values(await getAllItems());
+
+  let main = document.getElementById("menu");
+
+  let cardTemplate = html`${items.map((item) => {
+    html`<option value=${item._id}>${item.text}</option>`;
+  })}`;
+
+  render(cardTemplate, main);
+
+  document
+    .querySelector('input[type="submit"]')
+    .addEventListener("click", addItem);
+
+  async function addItem(event) {
+    event.preventDefault();
+
+    let text = document.getElementById("itemText").value;
+
+    const response = await fetch(url, {
+      method: "POST",
+      headers: { "Content-type": "application/json" },
+      body: JSON.stringify({ text }),
+    });
+
+    let data = await response.json();
+
+    items.push(data);
+
+    if (response.ok) {
+      cardTemplate = html`${items.map(
+        (item) => html`<option value=${item._id}>${item.text}</option>`
+      )}`;
+      render(cardTemplate, main);
+    }
+  }
+  getAllItems();
+});
