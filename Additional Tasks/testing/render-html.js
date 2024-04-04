a112d81dcf3cdd72d75dc8b13724e1250eb30e6f
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2024-04-04 12:23:00 +0300

    testing

--- Additional Tasks/testing/render-html.js
+++ Additional Tasks/testing/render-html.js
@@ -3,10 +3,10 @@
 import { html, render } from "./node_modules/lit-html/lit-html.js";
 
 const cardTemplate = (card) => html`<div class="contact-card">
-  <ul>
-    <li></li>
-  </ul>
-</div>
-<div class="info">
-  <h2>${card}
-</div>`;
+    <ul>
+      <li>placeholderLink</li>
+    </ul>
+  </div>
+  <div class="info">
+    <h2>${card}</h2>
+  </div>`;
