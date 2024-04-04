a8507afc1bcf2e9d54dd7bff5514269873249768
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2024-04-04 12:19:01 +0300

    testing

--- Additional Tasks/testing/render-html.js
+++ Additional Tasks/testing/render-html.js
@@ -1,3 +1,12 @@
 "use strict";
 
 import { render, html } from "../../";
+
+const cardTemplate = (card) => html`<div class="contact-card">
+  <ul>
+    <li></li>
+  </ul>
+</div>
+<div class="info">
+  <h2>${card}
+</div>`;
