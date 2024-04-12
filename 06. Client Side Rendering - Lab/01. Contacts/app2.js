1f42a857b3c7ed9c144020d9accbf01b20a03db4
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2024-04-12 13:55:42 +0300

    some more practice

--- 06. Client Side Rendering - Lab/01. Contacts/app2.js
+++ 06. Client Side Rendering - Lab/01. Contacts/app2.js
@@ -5,4 +5,8 @@ import { contacts } from "./contacts.js";
 
 const mainElements = document.querySelector("#contacts");
 
-const contactTemplate = (contact) => html``;
+const contactTemplate = (contact) => html`<div class="contact-card>
+<div>
+<i class="far fa-user-circle gravatar"></i></div>
+<div class="info">
+<h2>Name: ${contact.name}</h2></div></div>`;
