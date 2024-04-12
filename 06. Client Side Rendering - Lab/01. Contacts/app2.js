1c082a22d71a712ec965c5f7b9b2a405d9219584
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2024-04-12 14:02:07 +0300

    some more practice

--- 06. Client Side Rendering - Lab/01. Contacts/app2.js
+++ 06. Client Side Rendering - Lab/01. Contacts/app2.js
@@ -5,19 +5,21 @@ import { contacts } from "./contacts.js";
 
 const mainElements = document.querySelector("#contacts");
 
-const contactTemplate = (contact) => html`<div class="contact card>
-<div>
-<i class="far fa-user-circle gravatar"></i></div>
-<div class="info">
-<h2>Name: ${contact.name}</h2>
-<button class ="detailsBtn">
-</button>
-<div class="details" id="1">
-<p>Phone Number ${contact.phone}</p>
-<p>Email ${contact.email}</p>
-</div></div></div>`;
+const contactTemplate = (contact) => html`<div class="contact card">
+  <div>
+    <i class="far fa-user-circle gravatar"></i>
+  </div>
+  <div class="info">
+    <h2>Name: ${contact.name}</h2>
+    <button class="detailsBtn">Details</button>
+    <div class="details" id="1">
+      <p>Phone Number ${contact.phone}</p>
+      <p>Email ${contact.email}</p>
+    </div>
+  </div>
+</div>`;
 
-render(contacts.map(contactTemplate), root);
+render(contacts.map(contactTemplate), mainElements);
 
 function showDetails(event) {
   const infoDiv = event.target.parentElement;
