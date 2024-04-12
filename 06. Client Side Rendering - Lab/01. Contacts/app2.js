6dfa9c27a2cbcf9911a230742dfcba2fc443aecd
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2024-04-12 13:53:45 +0300

    some more practice

---
+++ 06. Client Side Rendering - Lab/01. Contacts/app2.js
@@ -0,0 +1,8 @@
+"use strict";
+
+import { html, render } from "../../node_modules/lit-html/lit-html.js";
+import { contacts } from "./contacts.js";
+
+const mainElements = document.querySelector("#contacts");
+
+const contactTemplate = (contact) => html``;
