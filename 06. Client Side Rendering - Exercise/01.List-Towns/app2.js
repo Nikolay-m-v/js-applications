246553162684190af49583a8a5a94382333f7c68
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2024-04-18 11:59:41 +0300

    practice on client side rendering

--- 06. Client Side Rendering - Exercise/01.List-Towns/app2.js
+++ 06. Client Side Rendering - Exercise/01.List-Towns/app2.js
@@ -1,1 +1,7 @@
 "use strict";
+
+import { html, render } from "./node_modules/lit-html/lit-html.js";
+
+document.getElementById("btnLoadTowns").addEventListener("click", (event) => {
+  event.preventDefault();
+});
