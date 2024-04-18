65462f41849308dcf1d45240fea59ff16bb923d1
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2024-04-18 12:35:05 +0300

    npm node modules libraries practice

--- 06. Client Side Rendering - Exercise/01.List-Towns/app2.js
+++ 06. Client Side Rendering - Exercise/01.List-Towns/app2.js
@@ -4,4 +4,14 @@ import { html, render } from "./node_modules/lit-html/lit-html.js";
 
 document.getElementById("btnLoadTowns").addEventListener("click", (event) => {
   event.preventDefault();
+
+  let input = document.getElementById("towns").value;
+  let townsList = input.split(", ");
+
+  const townTemplate = html`<ul>
+    ${townsList.map((town) => html`<li>${town}</li>`)}
+  </ul>`;
+
+  let root = document.getElementById("root");
+  render(townTemplate, root);
 });
