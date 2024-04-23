199683e11e864d710d0436b019ba327dda5ed0b5
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2024-04-23 16:01:14 +0300

    practice

--- 06. Client Side Rendering - Exercise/06.Book-Library/app1.js
+++ 06. Client Side Rendering - Exercise/06.Book-Library/app1.js
@@ -4,3 +4,15 @@ import {
   render,
   html,
 } from "../03.Search-in-List/node_modules/lit-html/lit-html.js";
+
+const root = document.querySelector("body");
+const loadAllBooksButton = document.createElement("button");
+loadAllBooksButton.id = "loadBooks";
+loadAllBooksButton.textContent = "LOAD ALL BOOKS";
+
+loadAllBooksButton.addEventListener("click", loadBooks);
+root.appendChild(loadAllBooksButton);
+
+async function loadBooks(event) {
+  event.preventDefault;
+}
