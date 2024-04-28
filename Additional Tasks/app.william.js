eab07648d931c29eba71441fcacb22457c0af4fc
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2024-04-28 20:57:16 +0300

    practice with WIlliam

--- Additional Tasks/app.william.js
+++ Additional Tasks/app.william.js
@@ -1,2 +1,64 @@
 "use strict";
 /// coding with william
+import { html, render } from "./node_modules/lit-html/lit-html.js";
+
+const state = {};
+
+async function loadBooks() {
+  const url = "http://localhost:3030/jsonstore/collections/books";
+
+  const response = await fetch(url);
+  const data = await response.json();
+
+  return data;
+}
+
+function renderBookRow(book) {
+  return html` <tr>
+    <td>${book.title}</td>
+    <td>${book.author}</td>
+    <td>
+      <button>Edit</button>
+      <button>Delete</button>
+    </td>
+  </tr>`;
+}
+
+function renderBooksTable(containerElement) {
+  const booksTableTemplate = html` <table>
+    <thead>
+      <tr>
+        <th>Title</th>
+        <th>Author</th>
+        <th>Action</th>
+      </tr>
+    </thead>
+    <tbody>
+      ${Object.values(state.books).map((book) => renderBookRow(book))}
+    </tbody>
+  </table>`;
+
+  render(booksTableTemplate, containerElement);
+}
+
+async function loadAndRenderBooks() {
+  const books = await loadBooks();
+
+  state.books = books;
+
+  renderBooksTable(document.getElementById("books-container"));
+}
+
+function renderLoadBooksButton() {
+  const loadButtonTemplate = html`<button
+    @click="${loadAndRenderBooks}"
+    id="loadBooks"
+    type="button"
+  >
+    LOAD ALL BOOKS
+  </button>`;
+
+  render(loadButtonTemplate, document.getElementById("actions"));
+}
+
+renderLoadBooksButton();
