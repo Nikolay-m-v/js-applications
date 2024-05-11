08f673fad604f3b835a54be69ac1169a46275eee
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2024-05-11 13:41:34 +0300

    practice

--- 06. Client Side Rendering - Exercise/03.Search-in-List/search3.js
+++ 06. Client Side Rendering - Exercise/03.Search-in-List/search3.js
@@ -4,8 +4,6 @@ import { html, render } from "./node_modules/lit-html/lit-html.js";
 import { towns } from "./towns.js";
 
 const townsElement = document.getElementById("towns");
-const searchText = document.getElementById("searchText");
-const result = document.getElementById("result");
 
 function renderCard() {
   let cardTemplate = html`<ul>
@@ -13,6 +11,24 @@ function renderCard() {
   </ul>`;
 
   render(cardTemplate, townsElement);
+}
+
+document.getElementById("searchButton").addEventListener("click", search);
+
+function search() {
+  const searchText = document.getElementById("searchText").value.toLowerCase();
+
+  const result = towns.filter((town) =>
+    town.toLowerCase().includes(searchText)
+  );
+
+  result.forEach((town) => {
+    const match = document.getElementById(town);
+    match.classList.add("active");
+  });
+
+  const resultHtml = document.getElementById("result");
+  resultHtml.textContent = `${result.length} matches found`;
 }
 
 renderCard();
