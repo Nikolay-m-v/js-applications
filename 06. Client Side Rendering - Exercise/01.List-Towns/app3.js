a9a0206afeb193827e9558a5763988a7abf97236
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2024-05-11 11:56:13 +0300

    practice

--- 06. Client Side Rendering - Exercise/01.List-Towns/app3.js
+++ 06. Client Side Rendering - Exercise/01.List-Towns/app3.js
@@ -6,4 +6,13 @@ document.getElementById("btnLoadTowns").addEventListener("click", (event) => {
   event.preventDefault();
 
   const rootElement = document.getElementById("root");
+
+  let towns = document.getElementById("towns").value;
+  let townsList = towns.split(", ");
+
+  let cardTemplate = html`<ul>
+    ${townsList.map((town) => html`<li>${town}</li>`)}
+  </ul>`;
+
+  render(cardTemplate, rootElement);
 });
