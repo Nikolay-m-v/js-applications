50a0ca94cd238d597eb4e420d0d029917003642c
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2024-05-18 13:44:03 +0300

    working on exam task

--- JS Applications Exam - 06 August 2022/src/app.js
+++ JS Applications Exam - 06 August 2022/src/app.js
@@ -1,5 +1,33 @@
-import { login } from "./login.js";
+import page from '../node_modules/page/page.mjs';
 
-const loginButton = document.getElementById("loginButton");
+import { addSession } from './middlewares/session.js';
+import { addRender } from './middlewares/render.js';
 
-loginButton.addEventListener("click", login);
+import { homePage } from './views/home.js';
+import { catalogPage } from './views/catalog.js';
+import { loginPage } from './views/login.js';
+import { registerPage } from './views/register.js';
+import { createPage } from './views/create.js';
+import { detailsPage } from './views/details.js';
+import { editPage } from './views/edit.js';
+
+import { logout } from './api/user.js';
+
+page(addSession);
+page(addRender);
+
+page('/', homePage);
+page('/catalog', catalogPage);
+page('/login', loginPage);
+page('/register', registerPage);
+page('/create', createPage);
+page('/details/:id', detailsPage);
+page('/edit/:id', editPage);
+page('/logout', onLogout);
+
+page.start();
+
+function onLogout(ctx) {
+	logout();
+	ctx.page.redirect('/catalog');
+}
