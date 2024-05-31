c6c3df3225a9e5751be1d23abf462ee119671e3a
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2024-05-31 10:34:17 +0300

    exam tasks

--- JS Applications Exam - 1 Apr 2023/app.js
+++ JS Applications Exam - 1 Apr 2023/app.js
@@ -4,17 +4,22 @@ import { html, render } from "./node_modules/lit-html/lit-html.js";
 
 const headerElement = document.querySelector("header");
 function renderNavBar() {
-  const navBar = html`<div id="logoContainer">
-    <a href="placeholder" 
-      <img src="placeholder" alt="fruitpedia logo"></img>
-    </a>
-  </div>
-  <div id="navigationLinks">
-    <a href="placeholder">Fruits</a>
-    <a href="placeholder">Search</a>
-    <a href="placeholder">Add Fruit</a>
-    <a href="placeholder">Logout</a>
-  </div>`;
+  const navBar = html` <div id="logoContainer">
+      <a href="placeholder">
+        <img
+          src="./images/logo.png"
+          alt="fruitpedia logo"
+          width="100px"
+          height="100px"
+        />
+      </a>
+    </div>
+    <div id="navigationLinks">
+      <a href="placeholder">Fruits</a>
+      <a href="placeholder">Search</a>
+      <a href="placeholder">Add Fruit</a>
+      <a href="placeholder">Logout</a>
+    </div>`;
 
   render(navBar, headerElement);
 }
