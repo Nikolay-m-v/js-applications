267bd960e8b6c9829c16bff2a01f4621ade21931
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2024-05-31 10:21:29 +0300

    exam tasks

---
+++ JS Applications Exam - 1 Apr 2023/app.js
@@ -0,0 +1,17 @@
+"use strict";
+
+import { html, render } from "./node_modules/lit-html/lit-html.js";
+
+function renderNavBar() {
+  const navBar = html`<div id="logoContainer">
+    <a href="placeholder" 
+      <img src="placeholder" alt="fruitpedia logo"></img>
+    </a>
+  </div>
+  <div id="navigationLinks">
+    <a href="placeholder">Fruits</a>
+    <a href="placeholder">Search</a>
+    <a href="placeholder">Add Fruit</a>
+    <a href="placeholder">Logout</a>
+  </div>`;
+}
