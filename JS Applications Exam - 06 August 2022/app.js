d7538f0c2869597d0d11a25aeac5b1383ff09a21
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2024-06-04 10:09:55 +0300

    working on exam task

--- JS Applications Exam - 06 August 2022/app.js
+++ JS Applications Exam - 06 August 2022/app.js
@@ -1,2 +1,39 @@
 "use strict";
-/// need to redo html by creating it dynamically
+
+import { html, render } from "./node_modules/lit-html/lit-html.js";
+
+const headerElement = document.querySelector("header");
+
+function getSessionToken() {
+  return localStorage.getItem("sessionToken");
+}
+
+function renderNavBar() {
+  const sessionToken = getSessionToken();
+  const isLoggedIn = !!sessionToken;
+
+  const navBar = html` <div id="logoContainer">
+      <a href="placeholder">
+        <img
+          src="./images/logo.png"
+          alt="fruitpedia logo"
+          width="100px"
+          height="100px"
+        />
+      </a>
+    </div>
+    <nav id="navigationLinks">
+      <a href="placeholder">Fruits</a>
+      <a href="placeholder">Search</a>
+      <a href="placeholder">Add Fruit</a>
+      ${isLoggedIn
+        ? html`<a href="placeholder" @click=${logout}>Logout</a>`
+        : html`<a href="placeholder" @click=${login}>Login</a>`}
+    </nav>`;
+
+  render(navBar, headerElement);
+}
+
+async function login() {}
+
+renderNavBar();
