9ad7bc74f74ada2978dd6ec9e47690dad00dc5e4
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2024-06-15 12:15:40 +0300

    finishing exam

--- JS Applications Exam - 1 Apr 2023/app.js
+++ JS Applications Exam - 1 Apr 2023/app.js
@@ -36,9 +36,7 @@ function renderNavBar() {
   render(navBar, headerElement);
 }
 
-function renderMainPage(event) {
-  event.preventDefault();
-
+function renderMainPage() {
   const mainPage = html` <div id="mainWrapper">
     <h2>Learn More About Your Favorite Fruits</h2>
     <img src="./images/pexels-pixabay-161559-dImkWBDHz-transformed (1).png"
@@ -125,5 +123,3 @@ async function login(event) {
 function createAccount() {}
 
 renderNavBar();
-
-/// sess will
