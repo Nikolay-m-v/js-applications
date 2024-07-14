20743bb11d66d281d214f9b7a9e3aaa6d5f7d2e5
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2024-07-14 15:40:36 +0300

    working

--- JS Applications Exam - 2 April 2022/app.js
+++ JS Applications Exam - 2 April 2022/app.js
@@ -1,1 +1,35 @@
 "use strict";
+
+import { eventNames } from "process";
+import { html, render } from "./node_modules/lit-html/lit-html.js";
+
+(function () {
+  function renderHomepage(elements) {
+    const renderedHomepage = html`<img
+      src="./images/header-bg.png"
+      alt="red background"
+      width="100%"
+      height="100%"
+    />`;
+
+    render(renderedHomepage, elements.contentElement);
+  }
+
+  function getElements() {
+    const homeNavigationLink = document.getElementsByClassName("home");
+    const contentElement = document.getElementsByClassName("content");
+
+    return {
+      homeNavigationLink,
+      contentElement,
+    };
+  }
+
+  function eventHandler() {
+    const elements = getElements();
+
+    elements.homeNavigationLink.addEventListener("click", renderHomepage);
+  }
+
+  eventHandler();
+})();
